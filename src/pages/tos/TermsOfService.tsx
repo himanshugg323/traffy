d94ffa72b9b00ab4711316775f6e1dc6ea55a991
himanshugg323 <161947401+himanshugg323@users.noreply.github.com>
2024-05-21 14:16:23 +0530

    Update TermsOfService.tsx

--- src/pages/tos/TermsOfService.tsx
+++ src/pages/tos/TermsOfService.tsx
@@ -34,7 +34,7 @@ export default function TermsOfService() {
 
                     <div id="terms" className="mb-12">
                         <p className="heading">1. Terms</p>
-                        <p className="desc">By accessing the website at https://www.simpletraffic.co, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
+                        <p className="desc">By accessing the website at https://www.traffy.co/, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
                     </div>
 
                     <div id="service-agreement" className="mb-12" >
